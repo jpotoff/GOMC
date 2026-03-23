e47c536b1b7782060699c48569184323e1f64df0
jpotoff <jpotoff@wayne.edu>
2026-03-23 18:02:38 -0400

    PME bug fixes

--- test_script9.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-# Let's test if FFTW's definition of C2R and R2C introduces an unexpected factor of 2 or N
-# In particular, numpy RFFT computes standard DFT. Let's assume C2R computes unnormalized IFFT.
-print("Checking normalization...")
